524e062814fbf11ff7dee73cd553121fa9acf286
Ohazulume C. Fabian <fabianohaz@gmail.com>
2022-10-12 22:59:46 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State Module for HBNB project. """
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String

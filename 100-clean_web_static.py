17ae186817c12c0dae4e583c2a25d8f1b331b46b
smylinwolf <fabianohaz@gmail.com>
2023-09-17 16:38:31 -0700

    Keep it clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Function that deploys """
+from fabric.api import *
+
+
+env.hosts = ['54.157.129.183', '52.91.125.123']
+env.user = "ubuntu"
+
+def do_clean(number=0):
+    """ Cleans old versions of web_static """
+
+    number = int(number)
+
+    if number < 1:
+        number = 1
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))

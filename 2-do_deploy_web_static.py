f15645f4f1203ddc91fccdc76a8d88ba1d1471d7
Ohazulume C. Fabian <fabianohaz@gmail.com>
2023-09-17 23:01:53 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,12 +2,37 @@
 # A fabric script that distributes an archive to my web servers
 
 from fabric.api import put, local, run, env
+from datetime import datetime
 import os
 
 env.hosts = ['54.157.129.183', '52.91.125.123']
 env.users = "ubuntu"
 
 
+def do_pack():
+    """ A function, using a fabric script to generate a tgz archive
+    from the the contents of the web_static """
+
+    try:
+        # Create a formatted timestamp
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+        # Create the archive_name
+        archive_name = "web_static_{}.tgz".format(timestamp)
+
+        # create the versions folder if it does not exist
+        local("mkdir -p versions")
+
+        # create an archive from the contents of web_static
+        local("tar -czvf versions/{} web_static".format(archive_name))
+
+        # Return the archive
+        return "versions/{}".format(archive_name)
+
+    except Exception:
+        # Return None if an Error Occurs while execution
+        return None
+        
 def do_deploy(archive_path):
     """ My function to deploy the archive """
 
